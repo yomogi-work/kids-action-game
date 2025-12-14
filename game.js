5a5cef3410cd0ae595c2efad3a008aaf307577a6
yomogi-work <yomogi.artemisia2023@gmail.com>
2025-12-14 19:08:03 +0900

    Refactor game logic and image loading sequence

--- game.js
+++ game.js
@@ -1,50 +1,67 @@
-const dogImg = new Image();
-dogImg.src = "2E9A0C29-5DBB-40D6-93EA-3D88B5FBCBD1.png";
-
-const bossImg = new Image();
-bossImg.src = "DA1EA695-9BC6-4BBA-ABC4-596E0E9207AE.png";
-
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-let y = 200;
-let velocity = 0;
-let gravity = 0.6;
-let obstacleX = 300;
-let score = 0;
+const dogImg = new Image();
+const bossImg = new Image();
 
-canvas.addEventListener("touchstart", jump);
+dogImg.src = "2E9A0C29-5DBB-40D6-93EA-3D88B5FBCBD1.png";
+bossImg.src = "DA1EA695-9BC6-4BBA-ABC4-596E0E9207AE.png";
 
-function jump() {
-  velocity = -10;
-}
+let imagesLoaded = 0;
 
-function update() {
-  ctx.clearRect(0, 0, 300, 400);
+dogImg.onload = () => {
+  imagesLoaded++;
+  if (imagesLoaded === 2) startGame();
+};
 
-  // キャラ
-  velocity += gravity;
-  y += velocity;
-  ctx.drawImage(dogImg, 40, y, 40, 40); 
-  // 障害物
-  obstacleX -= 3;
-  if (obstacleX < -20) {
-    obstacleX = 300;
-    score++;
-  }
-  ctx.drawImage(bossImg, obstacleX, 280, 50, 120);
+bossImg.onload = () => {
+  imagesLoaded++;
+  if (imagesLoaded === 2) startGame();
+};
 
-  // 当たり判定
-  if (
-  40 < obstacleX + 50 &&
-  40 + 40 > obstacleX &&
-  y + 40 > 280
-  ) {
-    alert("ゲームオーバー！ スコア：" + score);
-    location.reload();
+function startGame() {
+  let y = 200;
+  let velocity = 0;
+  let gravity = 0.6;
+  let obstacleX = 300;
+  let score = 0;
+
+  canvas.addEventListener("touchstart", jump);
+
+  function jump() {
+    velocity = -10;
   }
 
-  requestAnimationFrame(update);
+  function update() {
+    ctx.clearRect(0, 0, 300, 400);
+
+    // 犬
+    velocity += gravity;
+    y += velocity;
+    ctx.drawImage(dogImg, 40, y, 40, 40);
+
+    // ラスボス
+    obstacleX -= 3;
+    if (obstacleX < -50) {
+      obstacleX = 300;
+      score++;
+    }
+    ctx.drawImage(bossImg, obstacleX, 280, 50, 120);
+
+    // 当たり判定
+    if (
+      40 < obstacleX + 50 &&
+      40 + 40 > obstacleX &&
+      y + 40 > 280
+    ) {
+      alert("ゲームオーバー！ スコア：" + score);
+      location.reload();
+    }
+
+    requestAnimationFrame(update);
+  }
+
+  update();
 }
 
 update();
